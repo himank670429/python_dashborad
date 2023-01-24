8967b4bd6eda9dfb319de0fe754f8100810c29ce
himank singh <singhhimank241@gmail.com>
2023-01-24 21:00:10 +0530

    added dummy data to be displayed

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
-# import pandas as pd
-# import plotly.express as px
-import dash
-from dash import dcc, html
-from dash.dependencies import Input, Output
+from dash import Dash, html, dcc, dash_table
+import plotly.express as px
+import pandas as pd
 
-app = dash.Dash(__name__)
+# creating our app
+app = Dash(__name__)
 
+# importing the data
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+# creating fig
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group") # bar graph
+
+# layout
 app.layout = html.Div(
-    html.H1(children='Hello, World!')
+    children=[
+        html.H1(children="hello Dash"),
+        dcc.Graph(
+            id="example-graph",
+            figure=fig
+        )
+    ]
 )
 
 if __name__ == "__main__":
     app.run_server(debug=True)
+    pass
