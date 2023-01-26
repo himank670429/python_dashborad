6edfc0f71c58d6abe1a2e4fffe1c633242f64a7e
himank singh <singhhimank241@gmail.com>
2023-01-26 20:12:41 +0530

    added function callback to update and render the data dynamically

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from dash import Dash, html
+from dash import Dash, html, Input, Output, callback
 import pandas as pd
 
-# just a comment
-# generating table for dash board
-def generate_tables(data_frame : pd.DataFrame):
+@callback(
+    Output(component_id='container',component_property='children'),
+    Input(component_id='btn', component_property='n_clicks'),
+)
+def generate_tables(n_clicks):
+    data_frame = pd.read_excel("data.xlsx")
     return html.Table([
         html.Thead(
             html.Tr([html.Th(col) for col in data_frame.columns])
@@ -16,19 +19,19 @@ def generate_tables(data_frame : pd.DataFrame):
         ])
     ])
 
+
+
 # creating app
 app = Dash(__name__)
-
-# import data
-df = pd.read_excel("data.xlsx")
 
 # app layout
 app.layout = html.Div(
     children = [
         html.H1(children = "Auction Of Bytes"),
-        generate_tables(df)
+        html.Div(id = "container", children = ""),
+        html.Button(id = 'btn',children = 'Refresh Data', n_clicks = 0)
     ]
 )
 
 if __name__ == "__main__":
-    app.run_server(debug = True)
+    app.run(debug = True)
