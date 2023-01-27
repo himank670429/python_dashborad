4a58c539e869b6d6a2880159d14d1ffb471a469e
himank singh <singhhimank241@gmail.com>
2023-01-27 20:06:36 +0530

    added compatibility for google sheets only

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 from dash import Dash, html, Input, Output, callback
 import pandas as pd
+from data import get_data
 
-url = r"https://docs.google.com/spreadsheets/d/e/2PACX-1vSzEDkDrgBh65x57aSFARUK_5jxlciUa_C8hZK5cDWeAe8Oo4j4ysypG0Ajl4RADpY7ZPdkwT14Y8dO/pub?gid=0&single=true&output=csv"
-
+url = "" # enter your google sheet url here
+sheet_name = "" # enter your google sheet name here
 @callback(
     Output(component_id='container',component_property='children'),
     Input(component_id='btn', component_property='n_clicks'),
 )
 
-
-
 def generate_tables(n_clicks):
-    # data_frame = pd.read_excel("data.xlsx")
-    data_frame = pd.read_csv("data.csv")
+    data_frame = get_data(url , sheet_name)
     return html.Table([
         html.Thead(
             html.Tr([html.Th(col) for col in data_frame.columns])
