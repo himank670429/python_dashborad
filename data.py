2ed9a89a260d3d5a2168b7a91e4c40b4006500c2
DeStRoYeR-droid <immanuelvivek40609@gmail.com>
2023-01-27 20:37:52 +0530

    Changes

--- data.py
+++ data.py
@@ -8,6 +8,8 @@ params -
 '''
 def get_data(url, sheet_name):
     sheet_id = url.split('/')[-2]
-    sheet_name = 'data'
     csv_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+    print (csv_url)
+    data_frame = pd.read_csv(csv_url)
+    print (data_frame)
     return pd.read_csv(csv_url)
--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from dash import Dash, html, Input, Output, callback
 import pandas as pd
 from data import get_data
 
-url = "" # enter your google sheet url here
-sheet_name = "" # enter your google sheet name here
+url = "https://docs.google.com/spreadsheets/d/1hL94OaViPEwfEcsm238wYnjA8L5bIkHvUdxfTcYYDBQ/edit#gid=0" # enter your google sheet url here
+sheet_name = "Data" # enter your google sheet name here
 @callback(
     Output(component_id='container',component_property='children'),
     Input(component_id='btn', component_property='n_clicks'),
