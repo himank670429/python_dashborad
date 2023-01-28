bc889308ba220e1e7fa0225bb67e971ac0609184
DeStRoYeR-droid <immanuelvivek40609@gmail.com>
2023-01-28 14:09:24 +0530

    Got Styling Working

--- stylingTest.py
+++ stylingTest.py
@@ -3,19 +3,19 @@ import pandas as pd
 import plotly.express as px
 from dash import dcc, html
 
+external_script = ["https://tailwindcss.com/", {"src": "https://cdn.tailwindcss.com"}]
+
 app = dash.Dash(
     __name__,
-    external_scripts="https://cdn.tailwindcss.com"
+    external_scripts=external_script
 )
-
 app.scripts.config.serve_locally = True
 
 app.layout = html.Div(
-    children=[
-        html.H1(children="Dash With Tailwind",
-        className="text-sky-400")
-    ]
+    children="Auction Of Bytes",
+    className="w-full h-screen bg-sky-200 font-sans text-8xl flex justify-center align-center text-cyan-400"
 )
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)
