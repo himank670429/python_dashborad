1f92895ddf8fc12c1e7dff7814a35e0e454598b1
himank singh <singhhimank241@gmail.com>
2023-01-28 21:08:00 +0530

    the dash now updated live(every 5 seconds)

--- data.py
+++ data.py
@@ -9,7 +9,7 @@ params -
 def get_data(url, sheet_name):
     sheet_id = url.split('/')[-2]
     csv_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
-    print (csv_url)
+    # print (csv_url)
     data_frame = pd.read_csv(csv_url)
-    print (data_frame)
+    # print (data_frame)
     return pd.read_csv(csv_url)
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from dash import Dash, html, Input, Output, callback
+from dash import Dash, html, Input, Output, callback, dcc
 import pandas as pd
 from data import get_data
 
 url = "https://docs.google.com/spreadsheets/d/1hL94OaViPEwfEcsm238wYnjA8L5bIkHvUdxfTcYYDBQ/edit#gid=0" # enter your google sheet url here
 sheet_name = "Data" # enter your google sheet name here
+reload_duration = 5 # duration of page refreshing 
+
 @callback(
     Output(component_id='container',component_property='children'),
-    Input(component_id='btn', component_property='n_clicks'),
+    Input(component_id='live-update', component_property='n_intervals'),
 )
 
-def generate_tables(n_clicks):
+def generate_tables(n_intervals):
     data_frame = get_data(url , sheet_name)
     return html.Table([
         html.Thead(
@@ -30,9 +32,13 @@ app = Dash(__name__)
 # app layout
 app.layout = html.Div(
     children = [
+        dcc.Interval(
+            id = 'live-update',
+            n_intervals = 0,
+            interval = reload_duration*1000
+        ),
         html.H1(children = "Auction Of Bytes"),
         html.Div(id = "container", children = ""),
-        html.Button(id = 'btn',children = 'Refresh Data', n_clicks = 0)
     ]
 )
 
