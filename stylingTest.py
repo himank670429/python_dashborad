9ac41b0d2073d12aa5b9635e609cb20df29a2d3d
DeStRoYeR-droid <immanuelvivek40609@gmail.com>
2023-01-28 21:23:11 +0530

    Tested styling

--- stylingTest.py
+++ stylingTest.py
@@ -13,7 +13,7 @@ app.scripts.config.serve_locally = True
 
 app.layout = html.Div(
     children="Auction Of Bytes",
-    className="w-full h-screen bg-sky-200 font-sans text-8xl flex justify-center align-center text-cyan-400"
+    className="w-full h-screen bg-cyan-400 font-sans text-8xl flex justify-center align-center text-[#002389]"
 )
 
 
